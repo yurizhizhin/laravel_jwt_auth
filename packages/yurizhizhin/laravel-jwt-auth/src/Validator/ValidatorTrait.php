d201402f3033035d4894c2667f815eace66b719d
Юрий Жижин <zhizhin1995@yandex.ru>
2022-12-19 17:39:31 +0400

    improved trait

--- packages/yurizhizhin/laravel-jwt-auth/src/Validator/ValidatorTrait.php
+++ packages/yurizhizhin/laravel-jwt-auth/src/Validator/ValidatorTrait.php
@@ -2,6 +2,7 @@
 
 namespace Yurizhizhin\LaravelJwtAuth\Validator;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use PHPOpenSourceSaver\JWTAuth\JWT;
 
@@ -12,16 +13,32 @@ use PHPOpenSourceSaver\JWTAuth\JWT;
 trait ValidatorTrait
 {
     /**
+     * @var string|bool $authToken
+     */
+    public string|bool $authToken;
+
+    /**
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->authToken = $request->server->get('HTTP_AUTHORIZATION', false);
+
+        if (is_string($this->authToken)) {
+            $this->authToken = str_replace('Bearer ', '', $this->authToken);
+        }
+    }
+
+    /**
      * Валидация токена
      *
-     * @param string $token
      * @return bool
      */
-    public function validateToken(string $token): bool
+    public function validateToken(): bool
     {
         /** @var JWTValidator $validator */
         $validator = App::make(JWT::class);
 
-        return $validator->validateToken($token);
+        return $validator->validateToken($this->authToken);
     }
 }
